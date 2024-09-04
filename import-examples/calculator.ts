357dfb0dc6db20da9b5a501cd3269327172888bd
Simon Verhoeven <simon.verhoeven@noreply.github.com>
2024-09-04 13:20:23 +0700

    start over

--- import-examples/calculator.ts
+++
@@ -1,4 +0,0 @@
-// Exporting a default function
-export default function calculateTotal(prices: number[]): number {
-  return prices.reduce((total, price) => total + price, 0);
-}
--- import-examples/mathUtils.ts
+++
@@ -1,9 +0,0 @@
-// Exporting a named function
-export function add(a: number, b: number): number {
-  return a + b;
-}
-
-// Exporting another named function
-export function subtract(a: number, b: number): number {
-  return a - b;
-}
