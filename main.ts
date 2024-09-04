f752ada0732f145664cf616e0abfceccaa07ea65
Simon Verhoeven <simon.verhoeven@noreply.github.com>
2024-09-04 15:34:01 +0700

    Creating text files

--- main.ts
+++ main.ts
@@ -1,11 +1,26 @@
 import { Construct } from 'constructs';
 import { App, TerraformStack } from 'cdktf';
+import { LocalProvider } from '@cdktf/provider-local/lib/provider';
+import * as path from 'path';
+import { file } from '@cdktf/provider-local';
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    // define resources here
+    // Initialize the local provider
+    new LocalProvider(this, 'local', {});
+
+    const projectDirectory = path.join(process.env.INIT_CWD!, './authors-projects');
+
+    const projectName = 'project-1';
+
+    const basePath = `${projectDirectory}/${projectName}`;
+
+    new file.File(this, 'ReadmeFile', {
+      filename: `${basePath}/README.md`,
+      content: `# ${projectName}\n\nThis is the ${projectName} project.`,
+    });
   }
 }
 
