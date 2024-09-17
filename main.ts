bf364ccdd50ddc877c4da0ad85e9476f6235ea9e
Simon Verhoeven <simon.verhoeven@noreply.github.com>
2024-09-17 20:36:49 +0700

    play around creating new stacks

--- main.ts
+++ main.ts
@@ -1,9 +1,15 @@
 import { App } from 'cdktf';
 import { NamePickerStack } from './stacks/NamePickerStack';
+import { WeekPlannerStack } from './stacks/WeekPlannerStack';
 
 export const PROJECT_NAME = 'cdktf-name-picker';
 export const BACKEND_NAME = `${PROJECT_NAME}-prereq`;
 
 const app = new App();
+
 new NamePickerStack(app, PROJECT_NAME);
+new NamePickerStack(app, PROJECT_NAME + '-prod');
+
+new WeekPlannerStack(app, 'cdktf-week-planner');
+
 app.synth();
