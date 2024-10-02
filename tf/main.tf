b1be7d6cde1bf324954084a1b893c41c549c99d5
Simon Verhoeven <simon.verhoeven@noreply.github.com>
2024-10-03 02:23:49 +0700

    cleanup

--- tf/main.tf
+++ tf/main.tf
@@ -3,10 +3,15 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
+# Random ID to ensure unique bucket name
+resource "random_id" "bucket_id" {
+  byte_length = 4
+}
+
+
 # Create an S3 bucket
 resource "aws_s3_bucket" "tf-demo-bucket-1" {
-  bucket = "tf-demo-bucket-1"
-
+  bucket              = "tf-demo-bucket-1-${random_id.bucket_id.hex}"
   object_lock_enabled = true
 }
 
@@ -14,6 +19,4 @@ module "s3_bucket" {
   source = "./modules/s3_bucket_with_env_tag"
   env    = "dev"
 }
-
-
 
--- tf/modules/s3_bucket_with_env_tag/main.tf
+++ tf/modules/s3_bucket_with_env_tag/main.tf
@@ -1,6 +1,12 @@
-resource "aws_s3_bucket" "tf-demo-bucket-2" {
-  bucket = "tf-demo-bucket-2" # Ensure unique bucket name
+# Random ID to ensure unique bucket name
+resource "random_id" "bucket_id" {
+  byte_length = 4
+}
 
+
+resource "aws_s3_bucket" "tf-demo-bucket-2" {
+  bucket              = "tf-demo-bucket-2-${random_id.bucket_id.hex}" # Ensure unique bucket name
+  object_lock_enabled = true
   tags = {
     Env = var.env
   }
