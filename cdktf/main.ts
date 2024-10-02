a69ca51ec7ae8435e835a8cbf08107ca300d8e24
Simon Verhoeven <simon.verhoeven@noreply.github.com>
2024-10-03 02:17:27 +0700

    added randomness

--- cdktf/main.ts
+++ cdktf/main.ts
@@ -1,14 +1,53 @@
 import { Construct } from 'constructs';
 import { App, TerraformStack } from 'cdktf';
+import { provider, s3Bucket } from '@cdktf/provider-aws';
+import * as random from '@cdktf/provider-random';
+
+class S3BucketWithEnvTag extends Construct {
+  constructor(scope: Construct, id: string, env: 'dev' | 'prod') {
+    super(scope, id);
+
+    const randomId = new random.id.Id(this, 'random-id', {
+      byteLength: 4,
+    });
+
+    // Create the S3 bucket
+    new s3Bucket.S3Bucket(this, 's3-bucket', {
+      bucket: `cdktf-demo-bucket-2-${randomId.hex}`,
+      objectLockEnabled: true,
+      tags: {
+        env: env,
+      },
+    });
+  }
+}
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    // define resources here
+    // Configure the AWS provider
+    new provider.AwsProvider(this, 'aws-provider', {
+      region: 'ap-southeast-1', // change to your preferred region
+    });
+
+    // Configure the random provider
+    new random.provider.RandomProvider(this, 'random-provider');
+
+    const randomId = new random.id.Id(this, 'random-id', {
+      byteLength: 4,
+    });
+
+    // Create the S3 bucket
+    new s3Bucket.S3Bucket(this, 's3-bucket', {
+      bucket: `cdktf-demo-bucket-1-${randomId.hex}`,
+      objectLockEnabled: true,
+    });
+
+    new S3BucketWithEnvTag(this, 's3-bucket-with-env-tag', 'dev');
   }
 }
 
 const app = new App();
-new MyStack(app, 'cdktf-project-builder');
+new MyStack(app, 'cdktf-demo');
 app.synth();
