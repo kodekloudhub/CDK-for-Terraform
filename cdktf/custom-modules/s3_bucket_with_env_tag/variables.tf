3eabc1e7d0dbc940ab54f87209beff6a070441a6
Simon Verhoeven <simon.verhoeven@noreply.github.com>
2024-10-03 02:52:34 +0700

    intro done

--- cdktf/custom-modules/s3_bucket_with_env_tag/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "env" {
-  description = "Environment tag for the bucket"
-  type        = string
-  validation {
-    condition     = var.env == "dev" || var.env == "prod"
-    error_message = "The env variable must be either 'dev' or 'prod'."
-  }
-}
